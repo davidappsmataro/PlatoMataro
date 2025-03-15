fe0fdd7ed74d4a98f3551068f667685c7dfbb917
David <david@grupcm.es>
2025-03-15 18:15:23 +0100

    Carrusel imágenes creado, sticky header hasta footer, menu responsive y opciones web implementadas

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -10,7 +10,54 @@
  * Write any other JavaScript below
  */
 
-+( function() {
-  const university = "UOC";
-  console.log(`Hello, ${university}!`);
-} )();
++(function () {
+  /****************************
+     MENÚ BURGUER & STICKY NAVBAR
+*****************************/
+
+  const opcionesMenu = document.querySelector(".header__navbar-menu");
+  const opcion = document.querySelectorAll(".header__navbar-menu-link");
+  const bBurguer = document.querySelector(".header__navbar-bburguer-btn");
+
+  //constrol stiky navbar
+  const header = document.querySelector(".header");
+  const stickyUntilFooter = document.querySelector(".footer");
+  const sticky = stickyUntilFooter.offsetTop;
+
+  const toggleMenuBurguer = () => {
+    opcionesMenu.classList.toggle("header__navbar-menu--active");
+  };
+
+  const opcionSeleccionada = (e) => {
+    //Si hemos pulsado una de las opciones devolvemos
+    //true para cerrar el menu burguer
+    let pulsadaOpcion = false;
+    opcion.forEach((opc) => {
+      if (opc.contains(e.target)) {
+        pulsadaOpcion = true;
+      }
+    });
+    return pulsadaOpcion;
+  };
+
+  bBurguer.addEventListener("click", toggleMenuBurguer);
+
+  document.onclick = (e) => {
+    console.log(e.target);
+    if (
+      (!opcionesMenu.contains(e.target) && !bBurguer.contains(e.target)) ||
+      opcionSeleccionada(e)
+    ) {
+      //si pulsamos fuera del menu lo cerramos
+      opcionesMenu.classList.remove("header__navbar-menu--active");
+    }
+  };
+  //hago scroll y el menu lo oculatamos cuando llegamos al footer
+  window.addEventListener("scroll", () => {
+    if (window.pageYOffset < sticky) {
+      header.classList.add("header--sticky");
+    } else {
+      header.classList.remove("header--sticky");
+    }
+  });
+})();
