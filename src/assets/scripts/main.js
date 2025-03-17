e95c2ee83e94f68a71d588ec694dbfa4f92b946a
David <david@grupcm.es>
2025-03-17 16:34:06 +0100

    Mejoras banner splide y ocultar menú sticky al llegar al id mataro

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -11,57 +11,56 @@
  */
 
 +(function () {
-  /****************************
+  document.addEventListener("DOMContentLoaded", function () {
+    /****************************
      MENÚ BURGUER & STICKY NAVBAR
 *****************************/
+    const opcionesMenu = document.querySelector(".header__navbar-menu");
+    const opcion = document.querySelectorAll(".header__navbar-menu-link");
+    const bBurguer = document.querySelector(".header__navbar-bburguer-btn");
 
-  const opcionesMenu = document.querySelector(".header__navbar-menu");
-  const opcion = document.querySelectorAll(".header__navbar-menu-link");
-  const bBurguer = document.querySelector(".header__navbar-bburguer-btn");
+    //constrol stiky navbar
+    const header = document.querySelector(".header");
+    const stickyUntilFooter = document.getElementById("mataro");
+    let sticky = stickyUntilFooter.offsetTop;
+    const toggleMenuBurguer = () => {
+      opcionesMenu.classList.toggle("header__navbar-menu--active");
+    };
 
-  //constrol stiky navbar
-  const header = document.querySelector(".header");
-  const stickyUntilFooter = document.querySelector(".footer");
-  const sticky = stickyUntilFooter.offsetTop;
+    const opcionSeleccionada = (e) => {
+      //Si hemos pulsado una de las opciones devolvemos
+      //true para cerrar el menu burguer
+      let pulsadaOpcion = false;
+      opcion.forEach((opc) => {
+        if (opc.contains(e.target)) {
+          pulsadaOpcion = true;
+        }
+      });
+      return pulsadaOpcion;
+    };
 
-  const toggleMenuBurguer = () => {
-    opcionesMenu.classList.toggle("header__navbar-menu--active");
-  };
+    bBurguer.addEventListener("click", toggleMenuBurguer);
 
-  const opcionSeleccionada = (e) => {
-    //Si hemos pulsado una de las opciones devolvemos
-    //true para cerrar el menu burguer
-    let pulsadaOpcion = false;
-    opcion.forEach((opc) => {
-      if (opc.contains(e.target)) {
-        pulsadaOpcion = true;
+    document.onclick = (e) => {
+      /*  */ if (
+        (!opcionesMenu.contains(e.target) && !bBurguer.contains(e.target)) ||
+        opcionSeleccionada(e)
+      ) {
+        //si pulsamos fuera del menu lo cerramos
+        opcionesMenu.classList.remove("header__navbar-menu--active");
+      }
+    };
+    //hago scroll y el menu lo oculatamos cuando llegamos al footer
+    window.addEventListener("scroll", () => {
+      if (window.pageYOffset < sticky) {
+        header.classList.add("header--sticky");
+      } else {
+        header.classList.remove("header--sticky");
       }
     });
-    return pulsadaOpcion;
-  };
-
-  bBurguer.addEventListener("click", toggleMenuBurguer);
-
-  document.onclick = (e) => {
-    console.log(e.target);
-    if (
-      (!opcionesMenu.contains(e.target) && !bBurguer.contains(e.target)) ||
-      opcionSeleccionada(e)
-    ) {
-      //si pulsamos fuera del menu lo cerramos
-      opcionesMenu.classList.remove("header__navbar-menu--active");
-    }
-  };
-  //hago scroll y el menu lo oculatamos cuando llegamos al footer
-  window.addEventListener("scroll", () => {
-    if (window.pageYOffset < sticky) {
-      header.classList.add("header--sticky");
-    } else {
-      header.classList.remove("header--sticky");
-    }
-  });
-  //recalculo la posicion del footer si redimensiono la ventana (responsive).
-  window.addEventListener("resize", () => {
-    sticky = stickyUntilFooter.offsetTop;
+    //recalculo la posicion del footer si redimensiono la ventana (responsive).
+    window.addEventListener("resize", () => {
+      sticky = stickyUntilFooter.offsetTop;
+    });
   });
 })();
